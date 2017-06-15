ef1919fc80ef854b3e83ea3fb99dd8bd4b53e7a6
abed23 <hdzieciol@gmail.com>
2017-06-15 20:25:31 +0700

    Step 7

--- app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
+++ app/src/main/java/apidez/com/lab1/activity/LoginActivity.java
@@ -2,17 +2,43 @@ package apidez.com.lab1.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import apidez.com.lab1.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView tvSignUp, tvForgotPassword;
+    private EditText edtEmail, edtPassword;
+    private Button btnLogin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // TODO: Insert your code
+        tvSignUp = (TextView) findViewById(R.id.tvSignUp);
+        tvForgotPassword = (TextView) findViewById(R.id.tvForgot);
+
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        edtPassword = (EditText) findViewById(R.id.edtPassword);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View view) {
+                                            Toast.makeText(LoginActivity.this , "Succes", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+        );
+
+
     }
 
     private void showToast(String message) {
